c2860f767f7508bf9160199a58974fd573b8965f
Ryan Ashton <40135996+Ryan-Ashton@users.noreply.github.com>
2019-10-15 11:26:46 +0800

    Updated Javascript

---
+++ static/main.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#calculate');
+
+console.log(button)
+button.addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log(event)
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
